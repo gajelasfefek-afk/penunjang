50b8c365486504ae026b4b586b437d9d07b6eb2e
gajelasfefek-afk <gajelasfefek@gmail.com>
2026-08-09 17:04:31 +0700

    Refactor script to log player details
    Removed webhook functionality and added player information logging.

--- sangar.lua
+++ sangar.lua
@@ -1,19 +1,10 @@
--- TEST SCRIPT
-local HttpService = game:GetService("HttpService")
-local webhook = "https://discord.com/api/webhooks/1440706799002189998/zVyfFMoV0BRgn3YFC97OXmb8WcbnHJBPX0j-zOsOi7w8j4lddLR4dCuRPgaPcniyKTyd"
+local Players = game:GetService("Players")
 
-local data = {
-    content = "Hello from Roblox!",
-    username = "TestBot"
-}
+print("=== Player di Server ===")
 
-local success, err = pcall(function()
-    local response = HttpService:PostAsync(webhook, HttpService:JSONEncode(data), Enum.HttpContentType.ApplicationJson)
-    print("Response:", response)
-end)
-
-if success then
-    print("✅ BERHASIL!")
-else
-    print("❌ GAGAL:", err)
+for _, player in ipairs(Players:GetPlayers()) do
+    print("Username :", player.Name)
+    print("Display  :", player.DisplayName)
+    print("UserId   :", player.UserId)
+    print("-----------------------")
 end
